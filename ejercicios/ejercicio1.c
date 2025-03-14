c6713af8888bb5849d1109d62832ae120330e3b2
basaldua-Josue7 <josuebasaldua71@gmail.com>
2025-03-13 20:07:50 -0600

    Update ejercicio1.c

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -3,10 +3,11 @@
 
 int main() {
     int n;
-    printf("Ingrese el tamaño del arreglo: ");
+    printf("Ingrese el tamano del arreglo: ");
+    scanf("%i", &n);
     
-
     int *arr;
+    arr = (int*)malloc(n *sizeof(int));
     if (arr == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         return 1;
@@ -14,15 +15,16 @@ int main() {
 
     printf("Ingrese %d valores:\n", n);
     for (int i = 0; i < n; i++) {
-        
+        scanf("%i", &arr[i]);
     }
 
     printf("El arreglo es: ");
     for (int i = 0; i < n; i++) {
-        
+        printf("%i ", arr[i]);
     }
     printf("\n");
+    printf("Liberando memoria\n");
+    free(arr);
 
-    
     return 0;
 }
